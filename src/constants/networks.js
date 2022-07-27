3ae105d5c6cc1496fbfa7cb42aca1a35da575e09
andithemudkip <andithemudkip@gmail.com>
2022-07-27 21:30:50 +0300

    fix: default to testnet

--- src/constants/networks.js
+++ src/constants/networks.js
@@ -6,7 +6,7 @@ export const networks = {
 			erc1155: "0xa87071a188e3e8d3e30f53a335ecc329d88026b7",
 			utility: "0xcedc1a9b6a3b0266f00d2e4198da6b6e434896ea",
 		},
-		backend: "https://testnet-api.tephra.app/",
+		backend: "https://testnet-api.tephra.app",
 		// backend: "http://localhost:8080",
 	},
 	godwoken_mainnet: {
@@ -16,9 +16,11 @@ export const networks = {
 			erc1155: "0xa87071a188e3e8d3e30f53a335ecc329d88026b7",
 			utility: "0xcedc1a9b6a3b0266f00d2e4198da6b6e434896ea",
 		},
-		backend: "https://testnet-api.tephra.app/",
+		backend: "https://testnet-api.tephra.app",
 	},
 };
 
-export const defaultNetwork =
-	process.env.REACT_APP_DEFAULT_NETWORK || "godwoken_testnet";
+// export const defaultNetwork =
+// 	process.env.REACT_APP_DEFAULT_NETWORK || "godwoken_testnet";
+
+export const defaultNetwork = "godwoken_testnet";
